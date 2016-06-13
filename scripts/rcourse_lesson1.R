0638f675a28ff2f8758f136d6f413a03715c0bbf
mrcreel <michaelrcreel@gmail.com>
2016-06-13 13:50:34 -0500

    Added a PDF

--- scripts/rcourse_lesson1.R
+++ scripts/rcourse_lesson1.R
@@ -23,3 +23,11 @@ tail(data_bl)
 xtabs(~group, data_bl)
 xtabs(~type, data_bl)
 
+## MAKE FIGURES ####
+# By group
+data.plot = ggplot(data, 
+                   aes(x = group, y = rt)) + geom_boxplot()
+pdf("figures/data.pdf")
+data.plot
+dev.off()
+
